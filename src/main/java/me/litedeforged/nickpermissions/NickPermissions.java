ab4f61beaf9c5388faaf85cdc9ea4ebb0bdb6984
ilyaSeven7 <ilia.moshtaghi83@gmail.com>
2024-03-26 00:14:20 +0330

    Start Message Added!

--- src/main/java/me/litedeforged/nickpermissions/NickPermissions.java
+++ src/main/java/me/litedeforged/nickpermissions/NickPermissions.java
@@ -1,5 +1,7 @@
 package me.litedeforged.nickpermissions;
 
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NickPermissions extends JavaPlugin {
@@ -7,6 +9,7 @@ public final class NickPermissions extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Bukkit.getServer().getConsoleSender().sendMessage(MiniMessage.miniMessage().deserialize("<green>Plugin Has Been Enable!"));
 
     }
 
