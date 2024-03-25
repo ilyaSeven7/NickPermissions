e38907658371fb119013c36f869cd5cab079ed14
ilyaSeven7 <ilia.moshtaghi83@gmail.com>
2024-03-26 00:06:54 +0330

    Plugin Created.

---
+++ src/main/java/me/litedeforged/nickpermissions/NickPermissions.java
@@ -0,0 +1,17 @@
+package me.litedeforged.nickpermissions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NickPermissions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
